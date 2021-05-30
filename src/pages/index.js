dfe5cd1c3f28faa1302d4b081f07eadcf22c7f7c
Ritis <jimmyelf@gmail.com>
2021-05-30 19:44:31 +0500

    Create class Api, set CORS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,16 @@
 import './index.css';
+import Api from '../scripts/components/Api'
+
+const api = new Api({
+    url: 'http://xkcd.com/',
+    headers: {
+        "content-type": "application/json"
+    },
+    cors: 'https://cors-anywhere.herokuapp.com/'
+})
+
+api.getDataCard()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => console.log(err))
---
+++ src/scripts/components/Api.js
@@ -0,0 +1,30 @@
+export default class Api {
+    constructor(config) {
+        this._url = config.url;
+        this._headers = config.headers;
+        this._cors = config.cors;
+    }
+
+    _checkResponse(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка ${res.status}`);
+    }
+
+    getDataCard() {
+        return fetch(this._cors+this._url+`info.0.json`, {
+            method: 'GET',
+            headers: this._headers
+        })
+        .then(this._checkResponse);
+    }
+
+    getDataCardNum(num) {
+        return fetch(this._cors+this._url+num+`info.0.json`, {
+            method: 'GET',
+            headers: this._headers
+        })
+        .then(this._checkResponse);
+    }
+}
